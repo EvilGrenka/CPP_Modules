25879de51c6ff9a4f2ee2408a29d89eb68d5d9e8
EvilGrenka <goodamoral@gmail.com>
2022-03-30 13:05:27 +0300

    some changes

---
+++ Module00/ex01/PhoneBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnoriko <rnoriko@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 12:45:52 by rnoriko           #+#    #+#             */
+/*   Updated: 2022/03/30 12:45:52 by rnoriko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+
+class PhoneBook
+{
+    private:
+        static const int MAX_CONTACTS = 8;
+}
+
+
+#endif
